ba96d95e6624505cf5a246fe0ba1cba4ea82cd1f
dahyoji <gkdud8429@naver.com>
2025-05-30 14:31:40 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 6000,
+        speed: 1000,
+    });
+
+
+    $('.main_visual .arrows .prev').on('click', function () {
+        $('.main_visual_slide').slick('slickPrev');
+    });
+
+    $('.main_visual .arrows .next').on('click', function () {
+        $('.main_visual_slide').slick('slickNext');
+    });
+
+    $('.main_living_slide').slick({
+        slidesToShow: 4,
+        arrows: false,
+        dots: true,
+    });
+
+
+    $('.main_living .arrows .prev').on('click', function () {
+        $('.main_living_slide').slick('slickPrev');
+    });
+
+    $('.main_living .arrows .next').on('click', function () {
+        $('.main_living_slide').slick('slickNext');
+    });
+
+});
+
+$(function () {
+    $('.main_content .tab_menu button').on('click', function () {
+        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
+        console.log(idx);
+
+        $('.main_content .tab_menu button').removeClass('on');
+        $(this).addClass('on');
+
+        $('.main_content .tab_content .con').removeClass('on');
+        $('.main_content .tab_content .con').eq(idx).addClass('on');
+    });
+});
+
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 })
+    });
+
+    $(window).on('scroll', function () {
+        //스크롤 된 양을 구함.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+
+        if (sct > 400) {
+            $('.to_top').addClass('on')
+        } else {
+            $('.to_top').removeClass('on')
+        }
+    });
+
+})
+
+$(function () {
+
+    $('.mbtn').on('click', function () {
+        $('.gnb').toggleClass('on')
+    });
+
+    $('.gnb>ul>li>a').on('click', function (e) {
+        if ($('.gnb').hasClass('on') && $(this).next().is('ul')) {
+            e.preventDefault();
+            $('.gnb>ul>li ul').stop().slideUp();
+            $(this).next().stop().slideToggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        let ww = $(window).width();
+        if (ww > 768) {
+            $('.gnb').removeClass('on');
+            $('.gnb>ul>li ul').removeAttr('style');
+        }
+    })
+})
